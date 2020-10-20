ffba50974bcf31fe030cb4a1715fbe28d278c5bd
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2020-10-20 17:44:11 -0400

    Updated profile table to include user role, favorites, address, and bannerUrl

--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -3,11 +3,16 @@ exports.up = (knex) => {
     .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable('profiles', function (table) {
       table.string('id').notNullable().unique().primary();
+      table.string('role');
       table.string('email');
       table.string('name');
       table.string('avatarUrl');
+      table.string('bannerUrl');
+      table.string('address');
+      table.string('clientFavorites');
       table.timestamps(true, true);
-    });
+
+    })
 };
 
 exports.down = (knex) => {

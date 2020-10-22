47837ef71573418c374e63e8b73144b06c899b33
elijahmandeville <elijahmandeville@gmail.com>
2020-10-22 15:22:05 -0600

    create pets model with all necessary functions

--- api/pets/petsModel.js
+++ api/pets/petsModel.js
@@ -1,1 +1,38 @@
 const db = require('../../data/db-config');
+
+const findAll = async () => {
+  return await db('pets');
+}
+
+const findBy = (filter) => {
+  return db('pets').where({ filter });
+}
+
+const findByUserId = async (user_id) => {
+  return db('pets').where({ user_id }).select('*');
+}
+
+const addPet = async (pet) => {
+  return db('pets').insert(pet).returning('*');
+}
+
+const update = (id, pet) => {
+  return db('pets')
+    .where({ id: id })
+    .first()
+    .update(pet)
+    .returning('*');
+}
+
+const remove = async (id) => {
+  return await db('pets').where({ id }).del()
+}
+
+module.exports = {
+  findAll,
+  findBy,
+  findByUserId,
+  addPet,
+  update,
+  remove
+};

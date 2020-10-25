029083bc1d0bb5173a2bfd19bf4e7f15cb48ae93
elijahmandeville <elijahmandeville@gmail.com>
2020-10-25 17:55:41 -0600

    fixed linting errors with github

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,6 +1,6 @@
 const faker = require('faker');
 
-const roles = ['groomer', 'user']
+const roles = ['groomer', 'user'];
 
 const profiles = [...new Array(5)].map((i, idx) => ({
   id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
@@ -11,11 +11,9 @@ const profiles = [...new Array(5)].map((i, idx) => ({
       ? 'Test001 User'
       : `${faker.name.firstName()} ${faker.name.lastName()}`,
   role:
-    idx === 0
-      ? 'user'
-      : `${roles[Math.floor(Math.random() * roles.length)]}`,
+    idx === 0 ? 'user' : `${roles[Math.floor(Math.random() * roles.length)]}`,
   bannerUrl: faker.image.city(),
-  address: faker.address.streetAddress()
+  address: faker.address.streetAddress(),
 }));
 
 exports.seed = function (knex) {
